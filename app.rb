dc599337e9cdbca666d113c24836ee153b850aa9
Ami <shiratoriami@gmail.com>
2018-06-04 07:00:10 +0000

    rearranged keywords

--- app.rb
+++ app.rb
@@ -25,44 +25,53 @@ post '/callback' do
             when Line::Bot::Event::Message
                 case event.type
                     when Line::Bot::Event::MessageType::Text
-                        if event.message['text'] == '清算'
-                            if User.find(1).paid_total > User.find(2).paid_total
-                                total = User.find(1).paid_total - User.find(2).paid_total
-                                response_message = User.find(1).name + 'が' + total.to_s + '円多く払っていました。'
-                            elsif User.find(1).paid_total < User.find(2).paid_total
-                                total = User.find(2).paid_total - User.find(1).paid_total
-                                response_message = User.find(2).name + 'が' + total.to_s + '円多く払っていました。'
-                            else 
-                                response_message = 'ピタリ賞！清算はありません！'
+                        if event.message['text'].to_i == 0
+                            if event.message['text'] == 'キーワード'
+                                response_message = "確認\n清算終了\n金額(int)"
+                            elsif event.message['text'] == '確認'
+                                response_message = ''
+                                User.all.each do |user|
+                                    response_message << user.name + ':'
+                                    response_message << user.paid_total.to_s + '円'
+                                    response_message << "\n"
+                                end
+                                if User.find(1).paid_total > User.find(2).paid_total
+                                    total = User.find(1).paid_total - User.find(2).paid_total
+                                    response_message << User.find(1).name + 'が' + total.to_s + '円多く払っていました。'
+                                elsif User.find(1).paid_total < User.find(2).paid_total
+                                    total = User.find(2).paid_total - User.find(1).paid_total
+                                    response_message << User.find(2).name + 'が' + total.to_s + '円多く払っていました。'
+                                else 
+                                    response_message << 'ピタリ賞！清算はありません！'
+                                end
+                            elsif event.message['text'] == '清算終了'
+                                if User.find(1).paid_total > User.find(2).paid_total
+                                    total = User.find(1).paid_total - User.find(2).paid_total
+                                    response_message = User.find(1).name + 'が' + total.to_s + '円多く払っていました。'
+                                elsif User.find(1).paid_total < User.find(2).paid_total
+                                    total = User.find(2).paid_total - User.find(1).paid_total
+                                    response_message = User.find(2).name + 'が' + total.to_s + '円多く払っていました。'
+                                else 
+                                    response_message = 'ピタリ賞！清算はありません！'
+                                end
+                                User.all.each do |user| 
+                                    user.update_column(:paid_total, 0)
+                                end
+                                response_message << "\n清算額をリセットしました。"
+                            else
+                                response_message = '払った金額、清算、清算終了のどれかを送ってね！'
                             end
-                        elsif event.message['text'] == '清算終了'
-                            if User.find(1).paid_total > User.find(2).paid_total
-                                total = User.find(1).paid_total - User.find(2).paid_total
-                                response_message = User.find(1).name + 'が' + total.to_s + '円多く払っていました。'
-                            elsif User.find(1).paid_total < User.find(2).paid_total
-                                total = User.find(2).paid_total - User.find(1).paid_total
-                                response_message = User.find(2).name + 'が' + total.to_s + '円多く払っていました。'
-                            else 
-                                response_message = 'ピタリ賞！清算はありません！'
-                            end
-                            User.all.each do |user| 
-                                user.update_column(:paid_total, 0)
-                            end
-                            response_message << "\n清算額をリセットしました。"
-                        elsif event.message['text'].to_i == 0
-                            response_message = '払った金額、清算、清算終了のどれかを送ってね！'
                         else
                             paid = event.message['text'].to_i
-                            room_id = event['source']['roomId']
                             user_id = event['source']['userId']
                             
-                            url = "https://api.line.me/v2/bot/room/#{room_id}/member/#{user_id}"
+                            url = "https://api.line.me/v2/bot/profile/#{user_id}"
                             res = RestClient.get url, { :Authorization => "Bearer #{ENV['LINE_CHANNEL_TOKEN']}" }
                             returned_json = JSON.parse(res.body)
                             user_name =  returned_json["displayName"]
                             
                             user = User.find_by(name: user_name)
-                            user.update_column(:paid_total, user.paid_total + paid / 2)
+                            user.update_column(:paid_total, user.paid_total + (paid / 2))
                             response_message = user_name + 'は現在' + user.paid_total.to_s + '円立て替えています'
                         end
                         message = {
